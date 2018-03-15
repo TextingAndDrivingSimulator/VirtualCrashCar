bb06e42555b210e06339ff1aaec832d8d087dbfb
mbregg <matthewbregg@gmail.com>
2018-03-14 23:59:34 -0400

    Added a car which goes along a track

---
+++ Texting and Driving Simulation/Assets/ConstantlyRelerpPositionTo0.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantlyRelerpPositionTo0 : MonoBehaviour {
+
+	private Vector3 startPosition;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = this.transform.localPosition;
+	}
+	public float speed = .1f;
+	// Update is called once per frame
+	void Update () {
+		// Will technically never go to 0, but this is an easy lerp, and is close enough.
+		// Lerps should normaly be from a fixed start -> end, not a changing start -> end like in this one.
+		this.transform.localPosition = Vector3.Lerp (this.transform.localPosition, startPosition, speed);
+	}
+}
---
+++ Texting and Driving Simulation/Assets/ConstantlyRelerpRotationTo0.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantlyRelerpRotationTo0 : MonoBehaviour {
+	private Quaternion startRotation;
+
+	// Use this for initialization
+	void Start () {
+		startRotation = this.transform.localRotation;
+	}
+	public float speed = .1f;
+	// Update is called once per frame
+	void Update () {
+		// Will technically never go to 0, but this is an easy lerp, and is close enough.
+		// Lerps should normaly be from a fixed start -> end, not a changing start -> end like in this one.
+		this.transform.localRotation = Quaternion.Lerp (this.transform.localRotation, startRotation, speed);
+	}
+}
