3cffba2c6e2ef47f81a641cae29999221ff579f1
mbregg <matthewbregg@gmail.com>
2018-03-19 01:41:03 -0400

    Waypoints to end are created!

---
+++ Texting and Driving Simulation/Assets/LockXandZPosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockXandZPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPosition = new Vector3 (0, this.transform.localPosition.y, 0);
+		this.transform.localPosition = newPosition;
+	}
+}
