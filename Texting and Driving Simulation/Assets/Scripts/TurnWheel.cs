dbd9da8f5b5b0f7f24590b87ce381a0eeaf54824
JoshJohnson <jjohnson316@ufl.edu>
2018-03-21 21:54:00 -0400

    Repositioned str.wheel. Added scripts for controllers to grab objects. Began two different steering scripts.

---
+++ Texting and Driving Simulation/Assets/Scripts/TurnWheel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnWheel : MonoBehaviour {
+//****************** CITATION *********************************
+//Inspiration for wheel turning drawn from post on Unity forums
+//https://answers.unity.com/questions/1106170/how-to-make-a-functional-steering-wheel-for-a-car.html
+//*************************************************************
+	public int turnSpeed;
+	public GameObject wheel;
+
+	float rotations = 0;
+	Quaternion defaultRotation;
+
+	void Start()
+	{
+		defaultRotation = wheel.transform.localRotation;
+	}
+
+	void Update()
+	{
+		if(Input.GetKey (KeyCode.A)) //change to detect rotation position of wheel on x-axis
+		{
+
+			wheel.transform.Rotate(-Vector3.up * Time.deltaTime * turnSpeed);
+			rotations -= turnSpeed;
+		}
+
+		else if(Input.GetKey (KeyCode.D)) //change to detect rotation position of wheel on x-axis
+		{
+
+			wheel.transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+			rotations += turnSpeed;
+		}
+		else    //rotate to default
+		{
+			if (rotations < -1)
+			{
+				wheel.transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed);
+				rotations += turnSpeed;
+			}
+			else if (rotations > 1)
+			{
+				wheel.transform.Rotate(-Vector3.up * Time.deltaTime * turnSpeed);
+				rotations -= turnSpeed;
+			}
+			else if (rotations != 0)
+			{
+				wheel.transform.localRotation = defaultRotation;
+				rotations = 0;
+			}
+		}
+	}
+
+}
---
+++ Texting and Driving Simulation/Assets/Scripts/TurnWheelUpdated.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnWheelUpdated : MonoBehaviour {
+
+	public float smooth = 2.0F;
+	public float tiltAngle = 60.0F;
+
+	void Update () {
+		//FIX LOCAL XYZ POSITION
+
+		//Vector3 newPosition = new Vector3 (-0.406f, -0.119f, -3.54f);
+		//transform.localPosition = newPosition;
+
+		//Is this how I lock rotation about y and z?
+		//this.transform.localRotation = Quaternion.Euler(0,x,0);
+
+		//this.transform.localRotation.y = -90;
+		//this.transform.localRotation.z = 0;
+
+
+		//FIX LOCAL YZ ROTATION
+		float tiltAroundX = Input.GetAxis("Horizontal") * tiltAngle;
+		Quaternion target = Quaternion.Euler(tiltAroundX, 0 , 0); //is the order z, x, y?
+		this.transform.localRotation = Quaternion.Slerp(this.transform.localRotation, target, Time.deltaTime * smooth);
+
+	}
+}
