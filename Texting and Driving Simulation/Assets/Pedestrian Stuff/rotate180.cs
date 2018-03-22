869cf36be514875aa8361ee01f7ba4aae9094b73
Kyler Ferrell-Clegg <kclegg101@hotmail.com>
2018-03-22 10:00:26 -0400

    The 180 degree turn finally works but, only for 1st turn

---
+++ Texting and Driving Simulation/Assets/Pedestrian Stuff/rotate180.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate180 : MonoBehaviour {
+
+    public float distance1;
+    public float distance2;
+    public bool flip;
+
+	// Use this for initialization
+	void Start () {
+        distance1 = transform.position.x;
+        distance2 = transform.position.x;
+        flip = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //(transform.position.x <= -350 && transform.position.z <= 2084.64)
+        if (flip) { distance2 = transform.position.x; }
+
+        if ((distance1 - distance2 > 5 || distance2 - distance1 > 5) && distance2 < -346) // positon value we want to rotate character
+        {
+            transform.localRotation *= Quaternion.Euler(0, 180, 0);
+            distance2 = distance2+1;
+            flip = false;
+        }
+        else if((distance1 - distance2 > 5 || distance2 - distance1 > 5) && distance2 > -346)
+        {
+            transform.localRotation *= Quaternion.Euler(0, 180, 0);
+            distance2 = distance2 - 1;
+            flip = !flip;
+        }
+    }
+}
