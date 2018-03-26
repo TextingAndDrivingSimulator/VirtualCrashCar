f166c97c2a26c463614e5a3fd9ad002c41fa275e
mbregg <matthewbregg@gmail.com>
2018-03-26 14:45:39 -0400

    Added script to steer via vive controllers and have car respond.
    Testing now

--- Texting and Driving Simulation/Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
+++ Texting and Driving Simulation/Assets/Cartoon SportCar B01/Standard Assets/script/CarUserControl.cs
@@ -1,14 +1,26 @@
 using System;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using Valve.VR.InteractionSystem;
 
 namespace UnityStandardAssets.Vehicles.Car
 {
     [RequireComponent(typeof (CarController))]
     public class CarUserControl : MonoBehaviour
     {
+
+		private float normalizeToOne(float input) {
+			if (input < -1) {
+				return -1;
+			}
+			if (input > 1) {
+				return 1;
+			}
+			return input;
+		}
         private CarController m_Car; // the car controller we want to use
 
+		public CircularDrive drive;
 
         private void Awake()
         {
@@ -21,6 +33,18 @@ namespace UnityStandardAssets.Vehicles.Car
         {
             // pass the input to the car!
             float h = CrossPlatformInputManager.GetAxis("Horizontal");
+			Debug.Log ("Keyboard is " + h);
+
+			float wheel_rotation = drive.outAngle;
+			Debug.Log ("Raw wheel is " + wheel_rotation);
+			wheel_rotation /= 360.0f;
+			wheel_rotation = normalizeToOne (wheel_rotation);
+			Debug.Log("Wheel is " + wheel_rotation);
+
+			h += wheel_rotation;
+			h = normalizeToOne (h);
+		    
+
             float v = CrossPlatformInputManager.GetAxis("Vertical");
 #if !MOBILE_INPUT
             float handbrake = CrossPlatformInputManager.GetAxis("Jump");
@@ -31,3 +55,4 @@ namespace UnityStandardAssets.Vehicles.Car
         }
     }
 }
+
