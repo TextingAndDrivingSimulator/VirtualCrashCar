ab17f73ccf7b5aa57ed2c1d7f489980ac0c20c51
mbregg <matthewbregg@gmail.com>
2018-04-02 14:16:35 -0400

    Cars now pause when colliding into the user car

---
+++ Texting and Driving Simulation/Assets/StopCarOnEnter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopCarOnEnter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("found car!");
+		if (other.gameObject.CompareTag ("AICar")) {
+			MoveObjectAlongWaypoints mover = other.gameObject.GetComponentInParent<MoveObjectAlongWaypoints> ();
+			mover.paused = true;
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+		Debug.Log ("lost car!");
+		if (other.gameObject.CompareTag ("AICar")) {
+			MoveObjectAlongWaypoints mover = other.gameObject.GetComponentInParent<MoveObjectAlongWaypoints> ();
+			mover.paused = false;
+		}
+	}
+}
