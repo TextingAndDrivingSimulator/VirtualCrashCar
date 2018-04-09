0b451a78f883a597627321a8a7fd0a95cfe38e5e
mbregg <matthewbregg@gmail.com>
2018-04-09 00:53:09 -0400

    start scene now has pointers and is triggerd via vr buttons

---
+++ Texting and Driving Simulation/Assets/VRUIItem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+// From https://unity3d.college/2017/06/17/steamvr-laser-pointer-menus/
+
+[RequireComponent(typeof(RectTransform))]
+public class VRUIItem : MonoBehaviour
+{
+	private BoxCollider boxCollider;
+	private RectTransform rectTransform;
+
+	private void OnEnable()
+	{
+		ValidateCollider();
+	}
+
+	private void OnValidate()
+	{
+		ValidateCollider();
+	}
+
+	private void ValidateCollider()
+	{
+		rectTransform = GetComponent<RectTransform>();
+
+		boxCollider = GetComponent<BoxCollider>();
+		if (boxCollider == null)
+		{
+			boxCollider = gameObject.AddComponent<BoxCollider>();
+		}
+
+		boxCollider.size = rectTransform.sizeDelta;
+	}
+}
