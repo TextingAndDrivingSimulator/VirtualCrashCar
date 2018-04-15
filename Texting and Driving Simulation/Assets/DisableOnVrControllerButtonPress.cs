8ca4031baf6a4be7f2986ad710c0ca040bf7617c
mbregg <matthewbregg@gmail.com>
2018-04-15 17:16:34 -0400

    unity can toggle phone on/off with grip buttons now

---
+++ Texting and Driving Simulation/Assets/DisableOnVrControllerButtonPress.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnVrControllerButtonPress : MonoBehaviour {
+	public SteamVR_Controller.Device myDevice;
+	public GameObject toDisable;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Printing sticks");
+		string[] inputSticks = UnityEngine.Input.GetJoystickNames ();
+		for (int i = 0; i != inputSticks.Length; ++i) {
+			Debug.Log ("Stick number:" + i);
+			Debug.Log (inputSticks [i]);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float gripValue = Input.GetAxis ("GripButton");
+	
+		toDisable.SetActive (gripValue == 0);
+	
+	}
+}
