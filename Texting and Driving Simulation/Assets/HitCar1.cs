d4ddfd93a7a1dcb8c652502665037e1203179121
Dylan Bloyer <dbloyer@ufl.edu>
2018-04-15 21:36:43 -0400

    Finished making the victim story shown in the end credits match up with whatever the user hits.  Also, fixed a typo in one of the victim stories (changed 'driving' to 'driver').

---
+++ Texting and Driving Simulation/Assets/HitCar1.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitCar1 : MonoBehaviour {
+
+    public GameObject userCar;
+    private AudioSource crash;
+    public Text CarHit;
+    private bool justHitCar = false;
+    private bool notHit = true;
+
+    private void Start()
+    {
+        CarHit.enabled = false;
+        crash = userCar.GetComponent<AudioSource>();
+    }
+
+    IEnumerator flashTime(float time, Text text)
+    {
+        text.enabled = true;
+        yield return new WaitForSeconds(time);
+        text.enabled = false;
+    }
+
+    IEnumerator hitTimeCar() //Makes sure dermerits aren't constantly added for 1 colission.
+    {
+        justHitCar = true;
+        yield return new WaitForSeconds(3);
+        justHitCar = false;
+    }
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Player" && justHitCar == false)
+        {
+            if (notHit)
+            {
+                CreditManager.victimNumbers.Add(2);
+                notHit = false;
+            }
+            crash.Play();
+            GoingOffRoadDemeritCounter.setDemerits(10);
+            StartCoroutine(flashTime(3, CarHit)); //Change the first parameter of flashTime to change the amount of time the text is on.
+            StartCoroutine(hitTimeCar());
+        }
+    }
+
+}
---
+++ Texting and Driving Simulation/Assets/HitMale2.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HitMale2 : MonoBehaviour
+{
+    private bool justHit = false;
+    private bool notHit = true;
+    public Text PedHit;
+    private void Start()
+    {
+        PedHit.enabled = false;
+    }
+
+    IEnumerator flashTime(float time, Text text)
+    {
+        text.enabled = true;
+        yield return new WaitForSeconds(time);
+        text.enabled = false;
+    }
+
+    IEnumerator hitTime()
+    {
+        justHit = true;
+        yield return new WaitForSeconds(3);
+        justHit = false;
+    }
+
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Player" && justHit == false)
+        {
+            if (notHit)
+            {
+                CreditManager.victimNumbers.Add(6);
+                notHit = false;
+            }
+            GoingOffRoadDemeritCounter.setDemerits(20);
+            StartCoroutine(flashTime(3, PedHit)); //Change the first parameter of flashTime to change the amount of time the text is on.
+            StartCoroutine(hitTime());
+        }
+    }
+
+}
