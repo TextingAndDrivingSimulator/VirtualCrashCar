d4183cb9dd31a55d71847324bffc166e20d220b5
Dylan Bloyer <dbloyer@ufl.edu>
2018-04-21 19:20:57 -0400

    There is now a delay before the animation starts. Next I'm gonna tackle the loop to start.

---
+++ Texting and Driving Simulation/Assets/Scripts/Morph.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Morph : MonoBehaviour {
+
+    public Animator anim;
+    public GameObject victim;
+
+    IEnumerator startAnim()
+    {
+        yield return new WaitForSeconds(15);
+        anim.SetBool("open", true);
+    }
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetBool("open", false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (victim.activeSelf == true)
+        {
+            StartCoroutine(startAnim());
+        }
+
+
+	}
+}
